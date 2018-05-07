a62b789b8b9603ad3b74d88dd151de0ccb4e13c7
Luka Loboda <lobodaluka@gmail.com>
2018-05-07 22:20:05 +0200

    Dodano spreminjanje parametrov modela

--- RVPSemi/Assets/Scripts/InvertedPendulum.cs
+++ RVPSemi/Assets/Scripts/InvertedPendulum.cs
@@ -8,23 +8,35 @@ public class InvertedPendulum : MonoBehaviour {
 	public GameObject pendulum;
 	public GameObject cart;
 
-	float poleLength = 1.0f;
+	float poleLength;
 	public float poleTheta;
 	float poleThetaDot = 0f;
 	float poleThetaDotDot = 0f;
-	float pendulumMass = 1.0f;
+	float pendulumMass;
 
 	float cartX;
 	float cartXDot = 0f;
 	float cartXDotDot = 0f;
-	float cartMass = 1.0f;
+	float cartMass;
 	float cartForce = 0f;
-	float cartMu = 2f;
+	float cartMu = 5f;
+
+	float startX = 0f;
+	float startTheta = 20f;
+	float startPoleLength = 1.0f;
+	float startPoleWeight = 1.0f;
+	float startCartWeight = 2.0f;
 
 	// Use this for initialization
 	void Start () {
-		poleTheta = (pendulum.transform.localEulerAngles.y - 180) * Mathf.Deg2Rad;
-		cartX = cart.transform.position.x;
+		poleTheta = (startTheta - 180) * Mathf.Deg2Rad;
+		cartX = startX;
+		poleLength = startPoleLength;
+		pendulumMass = startPoleWeight;
+		cartMass = startCartWeight;
+
+		pendulum.transform.localRotation = Quaternion.Euler(pendulum.transform.localRotation.x, startTheta, pendulum.transform.localRotation.z);
+		cart.transform.position = new Vector3(cartX, cart.transform.position.y, cart.transform.position.z);
 	}
 
 	// Update is called once per frame
@@ -55,5 +67,36 @@ public class InvertedPendulum : MonoBehaviour {
 
 	public void SetForce(float f) {
 		cartForce = f;
+	}
+
+	public void SetPoleLength(float l) {
+		startPoleLength = l;
+	}
+
+	public void SetPoleWeight(float w) {
+		startPoleWeight = w;
+	}
+
+	public void SetCartWeight(float w) {
+		startCartWeight = w;
+	}
+
+	public void SetStartTheta(float t) {
+		startTheta = t;
+	}
+
+	public void Reset() {
+		cartX = startX;
+		poleTheta = (startTheta - 180) * Mathf.Deg2Rad;
+		poleLength = startPoleLength;
+		pendulumMass = startPoleWeight;
+		cartMass = startCartWeight;
+		pendulum.transform.localRotation = Quaternion.Euler(pendulum.transform.localRotation.x, startTheta, pendulum.transform.localRotation.z);
+		cart.transform.position = new Vector3(cartX, cart.transform.position.y, cart.transform.position.z);
+		cartXDot = 0f;
+		cartXDotDot = 0f;
+		poleThetaDot = 0f;
+		poleThetaDotDot = 0f;
+		cartForce = 0f;
 	}
 }
---
+++ RVPSemi/Assets/Scripts/Settings.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Settings : MonoBehaviour {
+
+	public InputField inputPoleLength;
+	public InputField inputPoleWeight;
+	public InputField inputCartWeight;
+	public InputField inputTheta;
+
+	public InvertedPendulum pendulum;
+
+	float poleLength;
+	float poleWeight;
+	float cartWeight;
+	float startTheta;
+
+	// Use this for initialization
+	void Start () {
+		poleLength = 1.0f;
+		poleWeight = 1.0f;
+		cartWeight = 2.0f;
+		startTheta = 20.0f;
+
+		pendulum.SetPoleLength (poleLength);
+		pendulum.SetPoleWeight (poleWeight);
+		pendulum.SetCartWeight (cartWeight);
+		pendulum.SetStartTheta (startTheta);
+
+		inputPoleLength.text = string.Format ("{0:0.00}", poleLength);
+		inputPoleWeight.text = string.Format ("{0:0.00}", poleWeight);
+		inputCartWeight.text = string.Format ("{0:0.00}", cartWeight);
+		inputTheta.text = string.Format ("{0:0.00}", startTheta);
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void UpdatePoleLength() {
+		poleLength = float.Parse (inputPoleLength.text);
+		if (poleLength < 0.1f || poleLength > 4.0f)
+			poleLength = 1.0f;
+		pendulum.SetPoleLength (poleLength);
+		inputPoleLength.text = string.Format ("{0:0.00}", poleLength);
+	}
+
+	public void UpdatePoleWeight() {
+		poleWeight = float.Parse (inputPoleWeight.text);
+		if (poleWeight < 0.5f || poleWeight > 10.0f)
+			poleWeight = 1.0f;
+		pendulum.SetPoleWeight (poleWeight);
+		inputPoleWeight.text = string.Format ("{0:0.00}", poleWeight);
+	}
+
+	public void UpdateCartWeight() {
+		cartWeight = float.Parse (inputCartWeight.text);
+		if (cartWeight < 0.5f || cartWeight > 10.0f)
+			cartWeight = 2.0f;
+		pendulum.SetCartWeight (cartWeight);
+		inputCartWeight.text = string.Format ("{0:0.00}", cartWeight);
+	}
+
+	public void UpdateStartTheta() {
+		startTheta = float.Parse (inputTheta.text);
+		pendulum.SetStartTheta (startTheta);
+		inputTheta.text = string.Format ("{0:0.00}", startTheta);
+	}
+}
